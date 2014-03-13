347d84a5eafe23c61a2b900e0f0352dba81e6e67
stevexdevine <github@devine.me.uk>
2014-03-13 00:10:33 +0000

    Initial Commit

---
+++ Git Test/DDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DDAppDelegate.h
+//  Git Test
+//
+//  Created by Steve on 13/03/2014.
+//  Copyright (c) 2014 SD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Git Test/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Git Test
+//
+//  Created by Steve on 13/03/2014.
+//  Copyright (c) 2014 SD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DDAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DDAppDelegate class]));
+    }
+}
