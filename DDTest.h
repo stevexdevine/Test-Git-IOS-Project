d86923e7b7b96734a4d178c1d471439797d5b314
stevexdevine <github@devine.me.uk>
2014-03-13 09:33:19 +0000

    add test class

---
+++ DDTest.h
@@ -0,0 +1,13 @@
+//
+//  DDTest.h
+//  Git Test
+//
+//  Created by Steve on 13/03/2014.
+//  Copyright (c) 2014 SD. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DDTest : NSObject
+
+@end
---
+++ DDTest.m
@@ -0,0 +1,13 @@
+//
+//  DDTest.m
+//  Git Test
+//
+//  Created by Steve on 13/03/2014.
+//  Copyright (c) 2014 SD. All rights reserved.
+//
+
+#import "DDTest.h"
+
+@implementation DDTest
+
+@end
